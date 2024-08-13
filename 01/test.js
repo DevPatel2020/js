8896b121c6032dd1876ded3778d31ba7babb3312
DevPatel2020 <118866329+DevPatel2020@users.noreply.github.com>
2024-08-13 11:37:08 +0000

    first time

---
+++ 01/test.js
@@ -0,0 +1,16 @@
+const acId = 12345
+let acName = "dev"
+var acNo = "22"
+acCity = "vadodara"
+let acX
+
+// acId = "54321" // can't chance const
+
+acName = "dk"
+acNo = "11"
+acCity = "ahemdavad"
+
+/* not use var bcz of block scope and functional scope*/
+
+
+console.table([acId, acName, acNo, acCity, acX])
